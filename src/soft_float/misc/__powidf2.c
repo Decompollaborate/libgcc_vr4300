d79ef44ac3b8b42bd6034c7dfad292955670ec9f
angie <angheloalf95@gmail.com>
2023-11-06 21:38:51 -0300

    exponentiation

---
+++ src/soft_float/misc/__powidf2.c
@@ -0,0 +1,24 @@
+#include "gcc_vr4300/types.h"
+
+#pragma weak __powidf2
+
+/**
+ * Raise `a` to the power of `b`
+ *
+ * https://gcc.gnu.org/onlinedocs/gccint/the-gcc-low-level-runtime-library/routines-for-floating-point-emulation.html#c.__powidf2
+ */
+float64 __powidf2(float64 a, int b) {
+    unsigned int n = (b < 0) ? -b : b;
+    float64 y = (n % 2 != 0) ? a : 1;
+
+    // Binary exponentiation
+    while (n >>= 1) {
+        a = a * a;
+
+        if (n % 2 != 0) {
+            y = y * a;
+        }
+    }
+
+    return (b < 0) ? (1 / y) : y;
+}
---
+++ src/soft_float/misc/__powisf2.c
@@ -0,0 +1,24 @@
+#include "gcc_vr4300/types.h"
+
+#pragma weak __powisf2
+
+/**
+ * Raise `a` to the power of `b`
+ *
+ * https://gcc.gnu.org/onlinedocs/gccint/the-gcc-low-level-runtime-library/routines-for-floating-point-emulation.html#c.__powisf2
+ */
+float32 __powisf2(float32 a, int b) {
+    unsigned int n = (b < 0) ? -b : b;
+    float32 y = (n % 2 != 0) ? a : 1;
+
+    // Binary exponentiation
+    while (n >>= 1) {
+        a = a * a;
+
+        if (n % 2 != 0) {
+            y = y * a;
+        }
+    }
+
+    return (b < 0) ? (1 / y) : y;
+}
